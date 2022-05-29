b5a4c372af2c8349f27b6c9a9aa1b202d9646286
Romanjan Das <romanjandas.mail@gmail.com>
2022-05-29 09:13:06 +0530

    initial commit

---
+++ EvaluateString.java
@@ -0,0 +1,269 @@
+//package com.romanjandas.calculatorwithsteps;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class EvaluateString{
+        private static final char PLUS='+';
+        private static final char MINUS='-';
+        private static final char DIVIDE='/';
+        private static final char MULTIPLY='x';
+        private static final char LEFTBRACKET='(';
+        private static final char RIGHTBRACKET=')';
+        private static final String DEC_PAT=".00000";
+        private static final String DEC_REP="";
+        private static final String ZERO="0";
+
+        private static String process_string="";
+        private static String temp_string="initial_random_string";
+        private static String answer="";
+        private static String left_of_equation="";
+        private static String right_of_equation="";
+        private static String temp_equation="random_temp_equation";
+
+        public static String steps="";
+  
+    public static String evaluate_string(String s){
+        while(!temp_equation.equals(s)){
+            temp_equation=s;
+
+           // Log.d("mytag", "\nstep 1: " + s);
+            process_string=look_for_brackets(s);
+
+           // Log.d("mytag", "step 3: " + process_string);
+            process_of_evaluation_of_simple_string();
+            s=left_of_equation+answer+right_of_equation;
+            left_of_equation="";right_of_equation="";
+           // Log.d("mytag", "step 4: " + answer);
+           // Log.d("mytag", "step 5: " + s);
+            //steps=steps+"\n"+s;
+            if(answer.equals(s)){
+                break;
+            }
+        }    
+        return s;
+    }
+
+    private static void process_of_evaluation_of_simple_string(){
+        while(temp_string!=process_string){
+            temp_string=process_string;
+            checkForMultiplyOrDivisionSign(process_string);        
+        }
+        process_string=simplifyString(process_string);
+        add_or_subtract(process_string);
+    }
+
+    private static void checkForMultiplyOrDivisionSign(String s){
+        int m=0;
+        while(m<s.length()){
+            if(DIVIDE==s.charAt(m)){
+                divide_or_multiply(m,true);
+                break;
+            }
+            if(MULTIPLY==s.charAt(m)){
+                divide_or_multiply(m,false);
+                break;
+            }
+            m++;
+        }
+    }
+
+    private static void divide_or_multiply(int i,boolean operator){
+        Pattern p=Pattern.compile(DEC_PAT);
+        int k,l; String leftString="",rightString="",leftOfResult="",rightOfResult=""; double leftNumber,rightNumber,resultNumber;
+        int j=i;
+        j=j-1;
+        while(true){
+            leftString=process_string.charAt(j)+leftString;
+            j=j-1;
+            if((j==-1) || process_string.charAt(j)==MINUS || process_string.charAt(j)==PLUS || process_string.charAt(j)==MULTIPLY || process_string.charAt(j)==DIVIDE){    
+                k=j;
+                break;
+            }
+        }
+        j=i;
+        j=j+1;
+        while(true){
+            rightString=rightString+process_string.charAt(j);
+            j=j+1;
+            if(j==process_string.length()){ 
+                l=j-1;
+                break;
+            }
+            if(process_string.charAt(j)==MINUS || process_string.charAt(j)==PLUS || process_string.charAt(j)==MULTIPLY || process_string.charAt(j)==DIVIDE){
+                l=j;
+                while(l<process_string.length()){
+                    rightOfResult=rightOfResult+process_string.charAt(l);
+                    l++;
+                }
+                break;
+            }
+        }
+
+
+        while(k>-1){
+            leftOfResult=process_string.charAt(k)+leftOfResult;
+            k=k-1;
+        }
+        leftNumber=Double.parseDouble(leftString);
+        rightNumber=Double.parseDouble(rightString);
+        if(operator){
+            resultNumber=leftNumber/rightNumber;
+                    process_string=leftOfResult+String.format("%.5f",resultNumber)+rightOfResult;
+                    Matcher m=p.matcher(simplifyString(left_of_equation+leftOfResult+String.format("%.5f",resultNumber)+rightOfResult+right_of_equation));
+                    if(!ZERO.equals(m.replaceAll(DEC_REP))){
+                        steps=steps+"\n"+m.replaceAll(DEC_REP);
+                    }
+
+        }
+        if(!operator){
+            resultNumber=leftNumber*rightNumber;
+                    process_string=leftOfResult+String.format("%.5f",resultNumber)+rightOfResult;
+                    Matcher m=p.matcher(simplifyString(left_of_equation+leftOfResult+String.format("%.5f",resultNumber)+rightOfResult+right_of_equation));
+                    if(!ZERO.equals(m.replaceAll(DEC_REP))){
+                        steps=steps+"\n"+m.replaceAll(DEC_REP);
+                    }
+        }
+    }
+
+    private static String simplifyString(String s){
+        int i=0; String tempString="";
+        /*
+        while(i<s.length()){
+            if(s.charAt(i)==PLUS && s.charAt(i+1)==MINUS){
+                tempString=tempString+"-";
+                i=i+2;
+            }
+            else if(s.charAt(i)==MINUS && s.charAt(i+1)==MINUS){
+                tempString=tempString+"+";
+                i=i+2;
+            }
+            else{
+                tempString=tempString+s.charAt(i);
+                i++;
+            }
+            
+        }
+        */
+        int k=0; boolean l=false;
+        while(i<s.length()){
+            if(s.charAt(i)!=PLUS && s.charAt(i)!=MINUS){
+                tempString=tempString+s.charAt(i);
+                i++;
+            }
+            else{
+                while(s.charAt(i)==PLUS || s.charAt(i)==MINUS){
+                    if(s.charAt(i)==MINUS){
+                        k++;
+                    }
+                    i++;
+                    l=true;
+                }
+                if(k%2!=0 && l){
+                    tempString=tempString+"-";
+                    k=0; l=false;
+                }
+                else if(k%2==0 && l){
+                    tempString=tempString+"+";
+                    k=0; l=false;
+                }
+            }
+        }
+        return tempString;
+    }
+
+    private static void add_or_subtract(String s){
+        Pattern p2=Pattern.compile(DEC_PAT);
+        int i=s.length()-1; String str=""; double num=0;
+        while(i!=-1){
+            str=s.charAt(i)+str;
+            if(MINUS==s.charAt(i) || PLUS==s.charAt(i) || i==0){
+                num=Double.parseDouble(str)+num;
+                str="";
+            }
+            i=i-1;
+        }
+        answer=String.format("%.5f",num);
+        int f=answer.length()-1; String temp=""; //String tempx="";
+        /*
+        while(f>-1){
+            temp=answer.charAt(f)+temp;
+            if(answer.charAt(f)=='.'){
+                Matcher m2=p2.matcher(temp);
+                temp=m2.replaceAll(DEC_REP);
+               // Log.d("mytag",temp+"  <- temp");
+                break;
+            }
+            f=f-1;
+        }
+        f=f-1;
+        */
+        while(answer.charAt(f)=='0'){
+            f=f-1;
+        }
+        if(answer.charAt(f)=='.'){
+            f=f-1;
+        }
+        while(f>-1){
+            temp=answer.charAt(f)+temp;
+            f=f-1;
+        }
+        /*
+        while(f>-1){
+            tempx=answer.charAt(f)+tempx;
+            f=f-1;
+        }
+       // Log.d("mytag",tempx+"  <--tempx");
+        answer=tempx+temp;
+        */
+        answer=temp;
+       // Log.d("mytag",answer+"  <-answer");
+        //Matcher m2=p2.matcher(answer);
+        //answer=m2.replaceAll(DEC_REP);
+    }
+
+    private static String look_for_brackets(String s){
+        int leftBracketPosition=0,rightBracketPosition=0,i=0;
+        String s1="";
+        while(true){
+            if(i<s.length()){
+                if(LEFTBRACKET==s.charAt(i)){
+                    leftBracketPosition=i;
+                }
+                else if(RIGHTBRACKET==s.charAt(i)){
+                    rightBracketPosition=i;
+                    s1=separate_the_string(leftBracketPosition,rightBracketPosition,s);
+                    break;
+                }
+
+            }
+            else{
+                s1=s;
+                break;
+            }
+            i++;
+        }
+        return s1;
+    }
+
+    private static String separate_the_string(int a,int b,String s){
+        String midStr="",leftStr="",rightStr="",evalStr="",joinedStr="";
+        int i=0,j=b+1;
+        while(i<a){
+            leftStr=leftStr+s.charAt(i);
+            i++;
+        }
+        while(j<s.length()){
+            rightStr=rightStr+s.charAt(j);
+            j++;
+        }
+        while(a<b-1){
+            a++;
+            midStr=midStr+s.charAt(a);
+        }
+       // Log.d("mytag", "step 2: " + leftStr + "   " + midStr + "   " + rightStr);
+        left_of_equation=leftStr;
+        right_of_equation=rightStr;
+        return midStr;
+    }
+    
+}
---
+++ StringFormation.java
@@ -0,0 +1,169 @@
+//package com.romanjandas.calculatorwithsteps;
+
+//import android.util.Log;
+//import android.widget.TextView;
+
+public class StringFormation{
+    public static String input_string="";
+    public static String result="";
+    private static final char PL='+';
+    private static final char MI='-';
+    private static final char DI='/';
+    private static final char MU='x';
+    private static final char LB='(';
+    private static final char RB=')';
+    private static final char EQ='=';
+    private static final char DE='.';
+    public static char p;
+    private static char c;
+    public static boolean no_key_pressed=true;
+    public static boolean allow=false;
+    public static int rbn=0,lbn=0;
+    public static boolean equal_is_pressed=false;
+
+    //TextView textView;
+
+    public static void verify_input(char x){
+        if(equal_is_pressed && cn(x)){
+            no_key_pressed=true; allow=false; input_string=""; EvaluateString.steps=""; equal_is_pressed=false;
+        }
+        else {
+            equal_is_pressed=false;
+        }
+        c=x;
+       // Log.d("mytag",Character.toString(x));
+                if(no_key_pressed){ // run this -> if no char is entered previously, only allow (,- and numbers
+                    if(c==LB||c==MI||cn(c)){
+                        p=c;
+                        no_key_pressed=false;
+                        allow=true;
+                        rbn=0;lbn=0;
+                        if(c==LB){lbn=1;}
+                    }
+                }
+                else{   // run this -> if a char is entered previously, checks elligibility of char c wrt to p,previous char
+                    if(c==LB){allow=alb();}
+                    if(c==RB){allow=arb();}
+                    if(c==DI){allow=adi();}
+                    if(c==MU){allow=amu();}
+                    if(c==PL){allow=apl();}
+                    if(c==MI){allow=ami();}
+                    if(c==DE){allow=ade();}
+                    if(cn(c)){allow=anu();}
+                    if(c==EQ){allow=aeq();}
+                } 
+                if(c!=EQ && allow){ // run this -> if elligibility of c wrt p is checked
+                    p=c;
+                    input_string=input_string+c;
+                   // Log.d("mytag",input_string);
+                }
+                else if(c==EQ && rbn==lbn && allow){ // displays the input_string
+                   // Log.d("mytag",rbn+","+lbn);
+                    try{
+                        EvaluateString.steps=input_string;
+                        result=EvaluateString.evaluate_string(input_string);
+                    }
+                    catch(Exception e){
+                        input_string="Some error occured";
+                    }
+                    //input_string="";
+                    //no_key_pressed=true;
+                    //allow=false;
+                    input_string=result;
+                    p=input_string.charAt(input_string.length()-1);
+                }
+    }
+
+    private static boolean cn(char x){ // check if it is a number
+        if(x=='0'||x=='1'||x=='2'||x=='3'||x=='4'||x=='5'||x=='6'||x=='7'||x=='8'||x=='9'){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private static boolean alb(){  // allow left brackt..
+        if(p==RB||p==DE||cn(p)){
+            return false;
+        }
+        else{
+            lbn++;
+            return true;
+        }
+    }
+
+    private static boolean arb(){  // allow right bracket
+        if(p==LB||p==DI||p==MU||p==PL||p==MI||p==DE|| rbn==lbn){
+            return false;
+        }
+        else{
+            rbn++;
+            return true;
+        }
+    }
+
+    private static boolean adi(){ // allow division
+        if(p==LB||p==DI||p==MU||p==PL||p==MI||p==DE){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    private static boolean amu(){ // allow multi
+        if(p==LB||p==DI||p==PL||p==MI||p==DE||p==MU){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    private static boolean apl(){ // allow plus
+        if(p==LB||p==DI||p==MU||p==MI||p==PL||p==DE){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    private static boolean ami(){ // allow minus
+        if(p==PL||p==MI||p==DE){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    private static boolean ade(){ // allow decimal
+        if(p==LB||p==RB||p==MU||p==DI||p==PL||p==MI||p==DE){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    private static boolean anu(){ // allow numbers
+        if(p==RB){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+    private static boolean aeq(){ //allow equal
+        if(p==LB||p==MU||p==DI||p==PL||p==MI||p==DE || rbn!=lbn){
+            c='0'; // removing the eq char
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
